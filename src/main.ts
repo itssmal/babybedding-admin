2a611d8040ffbd4456dae8f7f8095dc05b34f939
Sasha <wwesasha12@gmail.com>
2020-07-10 17:49:13 +0300

    first commit

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import '/Users/itssmal/Desktop/babybedding/admin/node_modules/materialize-css/dist/js/materialize.js'
+
 if (environment.production) {
   enableProdMode();
 }
